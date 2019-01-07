73c122911575173effdf9b8dd20a89b8be5e1c45
No One <925821946@qq.com>
2019-01-07 12:21:14 +0800

    2

--- prediction.py
+++ prediction.py
@@ -8,12 +8,26 @@
 """
 from tensorflow import keras
 from Init import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 # load model
 model = keras.models.load_model('model/model' + str(epochs) + '.h5')
 
-# predictions shape (10000,10)
+# predictions: shape (10000,10)
 predictions = model.predict(test_images)
 
-print(predictions[0])
-print(predictions.shape)
+
+if __name__=='__main__':
+    no=int(input("please input the picture's no in test dataset(0-9999):"))
+    plt.figure()
+    plt.imshow(test_images[no])
+    plt.colorbar()
+    plt.grid(False)
+    # get the result of prediction
+    category=class_names[predictions[0].tolist().index(1)]
+    plt.title('the category:'+category)
+    plt.show()
+
+# print(predictions[0].tolist().index(1))
+# print(predictions.shape)
